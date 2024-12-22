834ee5b4a7311943ffbe9556b8f00aa216d97904
Alif Ahmed <alifahmed102@gmail.com>
2024-12-22 18:48:34 +0600

    sidebbbrr

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -16,7 +16,13 @@ function MainRoutes() {
         <Sidebar />
 
         {/* Main Content */}
-        <main className="flex-1 h-screen p-4 bg-[#f7f7f9] ml-0 lg-custom:ml-60 mr-0 lg-custom:mr-24 overflow-y-auto">
+
+        <main className="flex-1 h-screen p-4 bg-[#f7f7f9] ml-0 lg-custom:ml-60 mr-0 lg-custom:mr-24 ">
+          <div className=' flex my-4 justify-between'>
+            <h1>Search</h1>
+
+            <h1>Profile</h1>
+          </div>
         <Routes>
             <Route path="/home/home1" element={<Home1 />} />
             <Route path="/about/about1" element={<About1 />} />

0418d75c202b3bb1320eeae4a480fb50f0872418
Alif Ahmed <alifahmed102@gmail.com>
2024-12-22 15:22:51 +0600

    newsideebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FaHome, FaInfoCircle, FaEnvelope, FaChevronDown, FaChevronRight, FaSatellite, FaChartArea, FaUser, FaAddressBook, FaCashRegister } from 'react-icons/fa';
-import { GoCircle } from 'react-icons/go'; // Import the GoCircle icon
+import {  FaChevronDown, FaChevronRight, } from 'react-icons/fa';
 import { motion } from 'framer-motion'; // Import Framer Motion
 import ezi from '../assets/ezi.png';
 import ezicalc from '../assets/ezicalc.png';
 import { VscCircleFilled } from 'react-icons/vsc';
-import { FaCableCar, FaEarthAmericas } from 'react-icons/fa6';
+import { MdLockOutline } from 'react-icons/md';
+import { BsTelephoneForward } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
+import { IoCalculator } from 'react-icons/io5';
+import { LuUserRound } from 'react-icons/lu';
+import { GoHome } from 'react-icons/go';
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -15,36 +19,35 @@ const Sidebar = () => {
 
   const menuItems = [
     {
-      title: 'Home',
-      icon: <FaHome size={24} className="text-gray-600" />, 
+      title: 'Dashboards',
+      icon: <GoHome size={23} className="text-gray-600" />, 
       subItems: [
-        { title: 'Home 1', path: '/home/home1' },
-        { title: 'Home 2', path: '/home/home2' },
+        { title: 'E-Commerce', path: '/dashboards/ecommerce' },
+        { title: 'CRM', path: '/dashboards/crm' },
       ],
     },
     {
       title: 'Roles & Permissions',
-      icon: <FaAddressBook size={24} color='text-gray-600' />, // Increased icon size
+      icon: <MdLockOutline size={23} color='text-gray-600' />, 
       subItems: [
         { title: 'Roles', path: '/roles' },
         { title: 'Permissions', path: '/permission' },
       ],
     },
-    { title: 'Contact', icon: <FaEnvelope size={24} color='text-gray-600' />, path: '/contact' }, 
-    { title: 'Email', icon: <FaEarthAmericas size={24} color='text-gray-600' />, path: '/Email' }, 
+    { title: 'Contact', icon: <BsTelephoneForward size={23} color='text-gray-600' />, path: '/contact' }, 
+    { title: 'Email', icon: <AiOutlineMail size={23} color='text-gray-600' />, path: '/Email' }, 
     {
       title: "POS",
       path: "/pos",
-      icon: <FaCashRegister size={24} color='text-gray-600' />
+      icon: <IoCalculator size={23} color='text-gray-600' />
     },
     {
       title: 'Users',
-      icon: <FaUser size={24} color='text-gray-600' />,
+      icon: <LuUserRound size={23} color='text-gray-600' />,
       subItems: [
         { title: 'Lists', path: '/users/list' },
       ],
     },
-    { title: 'Chat', icon: <FaChartArea size={24} color='text-gray-600' />, path: '/Chat' }, 
   ];
 
   const toggleMenu = (index) => {
@@ -63,11 +66,15 @@ const Sidebar = () => {
       initial={{ width: isCollapsed ? 64 : 240 }}
       animate={{ width: isCollapsed ? 64 : 240 }}
       transition={{ type: 'spring', stiffness: 50, damping: 15 }} 
-      className="h-screen bg-[#eaebee] flex flex-col"
+      className="h-screen bg-[#f7f7f8] shadow-inner   flex flex-col"
       onMouseEnter={() => setIsCollapsed(false)}
       onMouseLeave={() => setIsCollapsed(true)}
     >
-      <div className="flex items-center p-4 bg-[#eaebee]">
+     
+
+      {/* Menu Items */}
+      <div className="flex-1 ml-4 mt-4 overflow-y-auto">
+      <div className="flex ml-1 items-center mb-7  ">
         <img src={ezi} alt="Ezicalc Logo" className="w-8 h-8" />
         {!isCollapsed && (
           <span className="ml-2 text-xl font-bold">
@@ -75,91 +82,87 @@ const Sidebar = () => {
           </span>
         )}
       </div>
+      {menuItems.map((item, index) => {
+  const isSubmenuOpen = openMenus[index];
+  const isParentOrSubActive =
+    location.pathname === item.path ||
+    (item.subItems && item.subItems.some((sub) => location.pathname === sub.path));
 
-      {/* Menu Items */}
-      <div className="flex-1 ml-4 overflow-y-auto">
-        {menuItems.map((item, index) => {
-          const isSubmenuOpen = openMenus[index];
-          const isParentOrSubActive =
-            location.pathname === item.path ||
-            (item.subItems && item.subItems.some((sub) => location.pathname === sub.path));
+  return (
+    <div key={index} className="group">
+      {/* Add headings for "APP" and "Pages" */}
+      {index === 2 && !isCollapsed && (
+        <div className="mt-4 mb-2 text-sm  text-gray-500">APP</div>
+      )}
+      {index === 5 && !isCollapsed && (
+        <div className="mt-4 mb-2 text-sm  text-gray-500">Pages</div>
+      )}
 
-          return (
-            <div key={index} className="group">
-              {item.subItems ? (
-                <div
-                  className={`flex items-center space-x-2 justify-between p-2 cursor-pointer transition ${
-                    isParentOrSubActive ? 'bg-gray-300 text-gray-800' : 'hover:bg-gray-300'
-                  } ${isCollapsed ? 'justify-center' : ''}`}
-                  onClick={() => toggleMenu(index)}
-                >
-                  <div className="flex items-center space-x-2">
-                    {item.icon}
-                    {!isCollapsed && <span className="whitespace-nowrap text-gray-600">{item.title}</span>}
-                  </div>
-                  {!isCollapsed && (
-                    <span className="">
-                      {isSubmenuOpen ? <FaChevronDown size={16} /> : <FaChevronRight size={16} />}
-                    </span>
-                  )}
-                </div>
-              ) : (
-                // <NavLink
-                //   to={item.path}
-                //   className={`flex items-center space-x-2 p-2 cursor-pointer transition ${
-                //     isParentOrSubActive ? 'bg-gray-300 text-gray-800 rounded-md' : 'hover:bg-gray-300 rounded-md'
-                //   } ${isCollapsed ? 'justify-start' : ''}`}
-                // >
-                //   {item.icon}
-                //   {!isCollapsed && <span>{item.title}</span>}
-                // </NavLink>
-
-                 <NavLink
-      to={item.path}
-      className={`flex items-center p-2 cursor-pointer transition ${
-        isParentOrSubActive ? 'bg-gray-300 text-gray-800 rounded-md' : 'hover:bg-gray-300 rounded-md'
-      } ${isCollapsed ? 'justify-start' : ''}`}
-    >
-      <div className="flex items-center space-x-2">
-        {item.icon}
-        <span
-          className={`transition-all text-gray-600 duration-300 ${
-            isCollapsed ? 'hidden' : 'whitespace-nowrap'
-          }`}
+      {item.subItems ? (
+        <div
+          className={`flex items-center space-x-2 justify-between p-2 cursor-pointer transition ${
+            isParentOrSubActive ? 'bg-gray-300 text-gray-800' : 'hover:bg-gray-300'
+          } ${isCollapsed ? 'justify-center' : ''}`}
+          onClick={() => toggleMenu(index)}
         >
-          {item.title}
-        </span>
-      </div>
-    </NavLink>
-              )}
+          <div className="flex items-center space-x-2">
+            {item.icon}
+            {!isCollapsed && <span className="whitespace-nowrap text-gray-600">{item.title}</span>}
+          </div>
+          {!isCollapsed && (
+            <span className="">
+              {isSubmenuOpen ? <FaChevronDown size={16} /> : <FaChevronRight size={16} />}
+            </span>
+          )}
+        </div>
+      ) : (
+        <NavLink
+          to={item.path}
+          className={`flex items-center p-2 cursor-pointer transition ${
+            isParentOrSubActive ? 'bg-gray-300 text-gray-800 rounded-md' : 'hover:bg-gray-300 rounded-md'
+          } ${isCollapsed ? 'justify-start' : ''}`}
+        >
+          <div className="flex items-center space-x-2">
+            {item.icon}
+            <span
+              className={`transition-all text-gray-600 duration-300 ${
+                isCollapsed ? 'hidden' : 'whitespace-nowrap'
+              }`}
+            >
+              {item.title}
+            </span>
+          </div>
+        </NavLink>
+      )}
 
-              {!isCollapsed && item.subItems && (
-                <motion.div
-                  initial={false}
-                  animate={isSubmenuOpen ? 'open' : 'closed'}
-                  variants={dropdownVariants}
-                  className="overflow-hidden"
-                >
-                  {item.subItems.map((subItem, subIndex) => {
-                    const isActive = location.pathname === subItem.path;
-                    return (
-                      <NavLink
-                        key={subIndex}
-                        to={subItem.path}
-                        className={`flex items-center space-x-2 p-2 text-sm ${
-                          isActive ? 'text-white bg-sky-500 rounded-md' : 'hover:bg-gray-300 rounded-md'
-                        }`}
-                      >
-                        <VscCircleFilled size={16} color={isActive ? 'white' : 'gray'} />
-                        <span>{subItem.title}</span>
-                      </NavLink>
-                    );
-                  })}
-                </motion.div>
-              )}
-            </div>
-          );
-        })}
+      {!isCollapsed && item.subItems && (
+        <motion.div
+          initial={false}
+          animate={isSubmenuOpen ? 'open' : 'closed'}
+          variants={dropdownVariants}
+          className="overflow-hidden"
+        >
+          {item.subItems.map((subItem, subIndex) => {
+            const isActive = location.pathname === subItem.path;
+            return (
+              <NavLink
+                key={subIndex}
+                to={subItem.path}
+                className={`flex items-center space-x-2 p-2 text-sm ${
+                  isActive ? 'text-white bg-sky-500 rounded-md' : 'hover:bg-gray-300 rounded-md'
+                }`}
+              >
+                <VscCircleFilled size={19} color={isActive ? 'white' : 'gray'} />
+                <span>{subItem.title}</span>
+              </NavLink>
+            );
+          })}
+        </motion.div>
+      )}
+    </div>
+  );
+})}
+
       </div>
     </motion.div>
   );
